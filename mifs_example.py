bddfc86115286f55d4337c642730050f364b5b6d
Alexandre Guichet <alexandre.g007@gmail.com>
2020-03-15 17:41:31 +0100

    Bug Fix: pd.Series implemetation, test on titanic dataset, comparaison with sklearn

--- mifs_example.py
+++ mifs_example.py
@@ -7,6 +7,37 @@ Created on Sun Mar 15 13:32:06 2020
 """
 
 from mifs import MIFS
+from mutual_information import mutual_information
+
+import numpy as np 
+import pandas as pd
 
 mifs = MIFS()
 mifs.load_pickle("titanic_train.pkl")
+
+features = mifs.df.drop(columns = ["Survived"])
+
+#Convert categorical
+cat_columns = features.select_dtypes(['category', 'object']).columns
+features[cat_columns] = features[cat_columns].astype('category').apply(lambda x: x.cat.codes)
+    
+labels = mifs.df["Survived"].to_frame()
+
+mi = mutual_information(features, labels, downsample = True)
+
+from sklearn.feature_selection import mutual_info_regression
+aa = dict()
+for i in features.columns:
+    x = features[i].values.reshape(1, -1)
+    y = labels.values.reshape(1, -1)
+    
+    missing_array = np.any(np.isnan(np.transpose(np.vstack([x, y]))), axis = 1) #TODO: Check if it returns 1D-array of booleans                
+    
+    x = x[np.invert(missing_array).reshape(1, -1)]
+    y = y[np.invert(missing_array).reshape(1, -1)]
+
+    aa[i] = mutual_info_regression(x.reshape(-1, 1), y.reshape(-1, 1))
+ 
+#Check what is the value of 1-to-1 ratio    
+bb = mutual_information(features["Fare"], features["Fare"])
+cc = mutual_info_regression(features["Fare"].values.reshape(-1, 1), features["Fare"].values.reshape(-1, 1))
--- mutual_information.py
+++ mutual_information.py
@@ -15,7 +15,7 @@ from sklearn.preprocessing import scale
 from sklearn.neighbors import NearestNeighbors
 from scipy.special import digamma
 
-def mutual_information(feature, target, n_neighbors = 3, ordered = True, downsampling = False):
+def mutual_information(feature, target, n_neighbors = 3, ordered = True, downsample = False):
     """
     Compute Mutual Information using the k-neighbor approach for continuous-discrete mixtures
     _________
@@ -46,66 +46,82 @@ def mutual_information(feature, target, n_neighbors = 3, ordered = True, downsam
                 https://arxiv.org/abs/cond-mat/0305641
             	- W. Gao, S. Kannan, S. Oh, P. Viswanath, "Estimating Mutual Information for discrete-continuous mixtures". arXiv preprint, arXiv:1709.06212, 2017.
                 https://arxiv.org/pdf/1709.06212.pdf
-    """
-
-
-    try: 
-        _, mx = feature.shape
-        _, my = target.shape
-    except:
-        mx, = feature.shape #Cases: pd.Series (1D) and 1D nump.arrays
-        my, = target.shape #Cases: pd.Series (1D) and 1D nump.arrays
+    """   
     
-    mi = np.zeros([mx, my])
-    
-    try: 
-        mi_labels = target.columns
-    except:
-        mi_labels = ["Labels_" + str(i) for i in range(my)]
-    
-    if isinstance(feature, pd.DataFrame):
-        mi_features = feature.columns
-    else: 
+    if isinstance(feature, pd.Series):
+        mx = 1
+        mi_features = [feature.name]
+        feature = feature.values
+    elif isinstance(feature, (np.ndarray, np.generic)):
+        try: 
+            _, mx = feature.shape
+        except ValueError:
+            mx = 1
         mi_features = ["Feature_" + str(i) for i in range(mx)]
+    elif isinstance(feature, pd.DataFrame):
+        _, mx = feature.shape
+        mi_features = feature.columns
+        feature = feature.values
         
-    with tqdm(total = my*mx, desc=_desc, bar_format = "{1_bar}{bar}]| {n_fmt}/{total_fmt} [time left: {remianing}]", ascii = True) as pbar:
+    if isinstance(target, pd.Series):
+        my = 1
+        mi_labels = [target.name]
+        target = target.values
+    elif isinstance(target, (np.ndarray, np.generic)):
+        try: 
+            _, my = target.shape
+        except ValueError:
+            my = 1
+        mi_labels = ["Label_" + str(i) for i in range(my)]
+    elif isinstance(target, pd.DataFrame):
+        _, my = target.shape
+        mi_labels = target.columns
+        target = target.values
+            
+    mi = np.zeros([mx, my])
+             
+    with tqdm(total = my*mx, desc="computing regression", bar_format = "{l_bar}{bar}]| {n_fmt}/{total_fmt} [time left: {remaining}]", ascii = True) as pbar:
         for i in range(my):
-            mi[:,i] = _estimate_mi(X, y, n_neighbors = n_neighbors, downsampling = downsampling, pbar = pbar)
+            mi[:,i] = _estimate_mi(feature, target[:,i].reshape(-1, 1) if my > 1 else target.reshape(-1 ,1), n_neighbors = n_neighbors, downsample = downsample, pbar = pbar)                
         pbar.close()
     
     mi = pd.DataFrame(mi, columns = mi_labels, index = mi_features)
-    
+        
     if ordered:
         mi.sort_values(by = mi_labels[0])
     
     return mi
 
-def _estimate_mi(X, y, n_neighbors = 3, downsampling = False, pbar = None):
+def _estimate_mi(X, y, n_neighbors = 3, downsample = False, pbar = None):
     
-    n_samples, n_features = X.shape
+    try:
+        n_samples, n_features = X.shape
+    except ValueError:
+        n_samples = X.shape[0]
+        n_features = 1
+        
     ny = y.shape[0]
     
-    if n_samples != ny and downsampling == False:
+    if n_samples != ny and downsample == False:
         raise ValueError("The number of samples in X does not match y, use 'downsampling' to remove 'nan'.")
     
     #Scale and add noise to X
     X = scale(X)
-    X = X + 1e-10 * np.maximum(1, np.mean(np.abs(X), axis = 0)) * np.random.normal(size = [n_samples, 1])
-    
+    if n_features > 1:
+        X = X + 1e-10 * np.maximum(1, np.nanmean(np.abs(X), axis = 0)) * np.random.normal(size = [n_samples, 1])
+    else:
+        X = X + 1e-10 * np.maximum(1, np.nanmean(np.abs(X))) * np.random.normal(size = n_samples)
     #Scale and add noise to y
     y = scale(y)
-    y = y + 1e-10 * np.maximum(1, np.mean(np.abs(y))) * np.random.normal(size = n_samples)
+    y = y + 1e-10 * np.maximum(1, np.nanmean(np.abs(y))) * np.random.normal(size = [n_samples, 1])
 
     mi = np.zeros(n_features) 
     
     for i in range(n_features):
-        x = X[:, i]
-        if downsampling:
-            try:
-                missing_aray = np.isnan(np.hstack([x, y])) #TODO: Check if it returns 1D-array of booleans
-                [x, yi] = resample(x, y, missing_aray)
-            except:
-                yi = y              
+        x = X[:, i].reshape(-1, 1) if n_features > 1 else X
+        if downsample:
+            missing_array = np.any(np.isnan(np.hstack([x, y])), axis = 1) #TODO: Check if it returns 1D-array of booleans                
+            [x, yi] = resample(x, y, missing_array)
         else:
             yi = y
             
@@ -119,10 +135,12 @@ def _compute_mi(x, y, n_neighbors):
     n_samples = x.shape[0]
     x = x.reshape((-1, 1))    
     y = y.reshape((-1, 1))
+    xy = np.hstack([x,y])
     
     nn = NearestNeighbors(metric = 'chebyshev', n_neighbors = n_neighbors)
     
-    nn.fit(np.hstack([x,y]))
+    nn.fit(xy)
+        
     radius = nn.kneighbors()[0]
     radius = np.nextafter(radius[:, -1], 0)
     
@@ -137,13 +155,14 @@ def _compute_mi(x, y, n_neighbors):
     return max(0, mi)
 
 def resample(X, y, missing_array):
+    
     X = X[np.invert(missing_array)]
     y = y[np.invert(missing_array)]
     
     nnx = X.shape[0]
     nny = y.shape[0]
     
-    resample_size = 500
+    resample_size = 891
     if (nnx < resample_size) & (nny < resample_size) & (nnx == nny):
         return X, y
     else:
