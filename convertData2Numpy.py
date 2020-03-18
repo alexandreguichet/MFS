3c2824f93e7ee09fb5f5986038277ad48a93970a
adrienguichet <adrienguichet@gmail.com>
2020-03-18 13:36:01 +0100

    ajout de la gestion du nom des colonnes dans convertData2Numpy

--- convertData2Numpy.py
+++ convertData2Numpy.py
@@ -9,34 +9,34 @@ import pandas as pd
 
 x = np.array([2,3,1,0])
 
-def _is_pd_series(data):
+def _is_pd_series(data, column_name):
     mx = 1
     my = data.size
     column_names = [data.name]
     np_data = data.values
     return mx, my, column_names, np_data
 
-def _is_pd_dataframe(data):
+def _is_pd_dataframe(data, column_name):
     my, mx = data.shape
     column_names = data.columns
     np_data = data.values
     return mx, my, column_names, np_data
 
-def _is_numpy_array(data):
+def _is_numpy_array(data, column_name):
     data = np.array(data, ndmin=2)
     my, mx = data.shape
-    column_names = ["Feature_" + str(i) for i in range(mx)]
+    column_names = [column_name + "_" + str(i) for i in range(mx)]
     np_data = data
     return mx, my, column_names, np_data
     
-def convert_data_2_numpy(data):
+def convert_data_2_numpy(data, column_name="column"):
     if isinstance(data, pd.Series):
-        return _is_pd_series(data)
+        return _is_pd_series(data, column_name)
     elif isinstance(data, pd.DataFrame):
-        return _is_pd_dataframe(data)
+        return _is_pd_dataframe(data, column_name)
     elif isinstance(data, np.ndarray):
-        return _is_numpy_array(data)
+        return _is_numpy_array(data, column_name)
     else:
         raise ValueError("The input data type is neither: pd.Series or pd.DataFrame or np.ndarray")
         
-convert_data_2_numpy("0")
+convert_data_2_numpy(x, "feature")
